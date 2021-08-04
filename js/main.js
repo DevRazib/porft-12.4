9e80eeee6a52cd92a2ab213084608d31d5ffe7fd
Your Name <ahmedrazib477@gmail.com>
2021-08-04 15:22:02 +0600

    this is 4th portfolio

---
+++ js/main.js
@@ -0,0 +1,14 @@
+ AOS.init({
+     offset: 300,
+     duration: 1000,
+ });
+ //banner Owl-carousel
+ $(".owl-carousel").owlCarousel({
+     margin: 30,
+     loop: true,
+     autoplay: true,
+     center: true,
+     items: 1,
+     nav: true
+
+ });
